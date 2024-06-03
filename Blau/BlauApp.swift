85aebde6a824cd58947f59f422676e8b95bcac75
Joe Blau <im@joeblau.com>
2024-06-02 20:17:14 -0700

    Initial Commit

---
+++ Blau/BlauApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlauApp.swift
+//  Blau
+//
+//  Created by Joe Blau on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BlauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blau/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blau
+//
+//  Created by Joe Blau on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

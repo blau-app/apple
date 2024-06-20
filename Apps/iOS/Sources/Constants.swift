896b89792cc6c572b4d58a0eed9f436b89e51f2e
Joe Blau <im@joeblau.com>
2024-06-19 15:47:03 -1000

    feat: add entitlements

--- Apps/iOS/Sources/Constants.swift
+++ Apps/iOS/Sources/Constants.swift
@@ -3,3 +3,4 @@
 
 import Foundation
 
+let CAPSULE_API_KEY = "0f344e4f3efae94db02f6f133f06c0a9"
--- Apps/iOS/Sources/Views/OnboardingView.swift
+++ Apps/iOS/Sources/Views/OnboardingView.swift
@@ -1,17 +1,103 @@
 // OnboardingView.swift
 // Copyright (c) 2024 Party Labs, Inc
 
+import CapsuleSwift
 import SwiftUI
 
 struct OnboardingView: View {
+    enum FocusField: Hashable {
+        case email, code
+    }
+    
+    @Environment(\.authorizationController) private var authorizationController
+
+    @FocusState private var focusedField: FocusField?
+    
+    @State var email: String = ""
+    @State var showVerify: Bool = false
+    @State var code: String = ""
+    
+    @StateObject var capsule = Capsule(environment: .beta(jsBridgeUrl: nil),
+                                       apiKey: CAPSULE_API_KEY)
+
     @Binding var presentOnboard: Bool
     var body: some View {
-        Text("Welcome To Blau")
-        Button(action: {
-            presentOnboard = false
-        }, label: {
-            Text("Onboard")
-        }).buttonStyle(.borderedProminent)
+        NavigationStack {
+            ZStack {
+                CapsuleWebView(capsule: capsule).hidden()
+                VStack {
+                    Form {
+                        Section {
+                            TextField(text: $email) {
+                                Text("Email Address")
+                            }
+                            .focused($focusedField, equals: .email)
+                            .textContentType(.emailAddress)
+                            .autocorrectionDisabled()
+                            .autocapitalization(.none)
+                                
+                            Button {
+                                login(email: email)
+                            } label: {
+                                Text("Login")
+                            }.disabled(showVerify)
+                        }
+
+                        if showVerify {
+                            Section {
+                                TextField(text: $code) {
+                                    Text("Verification Code")
+                                }
+                                .focused($focusedField, equals: .code)
+                                .textContentType(.oneTimeCode)
+                                .keyboardType(.numberPad)
+                                Button {
+                                    verify(code: code)
+                                } label: {
+                                    Text("Verify")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Login")
+        }
+        .onAppear {
+            focusedField = .email
+        }
+    }
+
+    private func login(email: String) {
+        Task {
+            do {
+                let userExists = try await capsule.checkIfUserExists(email: email)
+                if userExists {
+                    return
+                }
+                try await capsule.createUser(email: email)
+                showVerify = true
+                focusedField = .code
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private func verify(code: String) {
+        Task {
+            do {
+                let biometricsId = try await capsule.verify(verificationCode: code)
+
+                try await capsule.generatePasskey(email: email,
+                                                  biometricsId: biometricsId,
+                                                  authorizationController: authorizationController)
+                try await capsule.createWallet(skipDistributable: false)
+
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 

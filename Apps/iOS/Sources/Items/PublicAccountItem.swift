7b56b0f9d3f862b3a1c89f20e1bb1206d11e40d6
Joe Blau <im@joeblau.com>
2024-06-27 17:29:25 -0700

    feat: implement public acocunts with ens parsing"

---
+++ Apps/iOS/Sources/Items/PublicAccountItem.swift
@@ -0,0 +1,18 @@
+//
+//  PublicAccountItem.swift
+//  Blau
+//
+//  Created by Joe Blau on 6/27/24.
+//
+
+import SwiftUI
+
+struct PublicAccountItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PublicAccountItem()
+}
